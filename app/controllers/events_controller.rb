e0221b8b1dd49e3dcfd8692d500e75d605631a09
Deb Bassett <deb@urbanwide.com>
2014-11-13 23:00:23 +0000

    Add new bootstrap stuff and make work with existing stuff

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,6 +10,7 @@ class EventsController < ApplicationController
     @count = @event.plants.count
     @average = @event.plants.average(:height).round
     @tallest = @event.plants.maximum(:height)
+    @top_3 = @event.plants.order('height desc').limit(3)
 
     render :template => 'events/dashboard', :layout => 'dashboard'
   end
