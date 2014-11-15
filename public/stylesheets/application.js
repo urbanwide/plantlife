ead93e1565ab8adfffa361cff2829d7f101b02cc
Deb Bassett <deb@urbanwide.com>
2014-11-15 11:52:23 +0000

    Biggify (and uglify :) )

--- public/stylesheets/application.js
+++
@@ -1,4 +0,0 @@
-window.setTimeout(function(){
-    $(".twitter-timeline").contents().find(".e-entry-title").css("font-size","100");
-    $(".twitter-timeline").contents().find(".tweet").css("font-size","100");
-  }, 1000);
