7e22fb6ce6def159108d76458b528fa425e0ae37
Deb Bassett <deb@urbanwide.com>
2014-11-15 09:55:26 +0000

    Stuff

---
+++ public/javascripts/application.js
@@ -0,0 +1,4 @@
+window.setTimeout(function(){
+    $(".twitter-timeline").contents().find(".e-entry-title").css("font-family","sans-serif");
+    $(".twitter-timeline").contents().find(".tweet").css("font-family","sans-serif");
+  }, 1000);
---
+++ public/stylesheets/application.js
@@ -0,0 +1,4 @@
+window.setTimeout(function(){
+    $(".twitter-timeline").contents().find(".e-entry-title").css("font-size","100");
+    $(".twitter-timeline").contents().find(".tweet").css("font-size","100");
+  }, 1000);
